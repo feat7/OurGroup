94f45fe8d58d0dd8a848eb28a36ff967fce6777a
feat7 <vinaykhobragade99@gmail.com>
2017-06-16 22:32:46 +0530

    modified:   index.android.js

--- index.android.js
+++ index.android.js
@@ -5,6 +5,7 @@ import {
   Text,
   View
 } from 'react-native';
+import { Router, Scene } from 'react-native-router-flux';
 
 import HomeScreen from './app/components/ui/HomeScreen'
 import Login from './app/components/ui/Login'
@@ -12,7 +13,12 @@ import Login from './app/components/ui/Login'
 export default class OurGroup extends Component {
   render() {
     return (
-      <HomeScreen />
+      <Router>
+        <Scene key="root">
+          <Scene key="HomeScreen" component={HomeScreen} title="OurGroup App" initial={true} />
+          <Scene key="Login" component={Login} title="Login" />
+        </Scene>
+      </Router>
     );
   }
 }
