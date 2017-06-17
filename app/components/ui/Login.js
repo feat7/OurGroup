9ebc739a59a9f18be06ca67ff0396a86c41dcfd7
feat7 <vinaykhobragade99@gmail.com>
2017-06-17 12:41:22 +0530

    FBSDK working fine

--- app/components/ui/Login.js
+++ app/components/ui/Login.js
@@ -5,7 +5,32 @@ import {
 	StyleSheet
 } from 'react-native'
 
+const FBSDK = require('react-native-fbsdk');
+const {
+  GraphRequest,
+  GraphRequestManager,
+} = FBSDK;
+
 export default class Login extends Component {
+
+	_responseInfoCallback(error: ?Object, result: ?Object) {
+	  if (error) {
+	    alert('Error fetching data: ' + JSON.stringify(error));
+	  } else {
+	    alert('Success fetching data: ' + JSON.stringify(result));
+	  }
+	}
+
+	componentDidMount() {
+		const infoRequest = new GraphRequest(
+		  '/1896202937282833/posts?fields=attachments&access_token=EAAaRRZCZBtKYMBADIkcH7XolJcfRqq1q4Gn7RPHZAESWCsV0iDjxfKrNjkENZCtHEZCygZBg8ZBytvnfbQfFkDq2AMrZBMoHUAGeru42C97DXqJUP5CyZBPk6p0c0k2gtZAAYXWVdD92GkfObcf3FoNeQTeeZAhBY7yZCk1YlbbTwtyIyvEHmhBXtbZAyoSOZAVhkTyesTZAzI0GNGmZCwZDZD',
+		  null
+		  ,
+		  this._responseInfoCallback,
+		);
+		      
+		new GraphRequestManager().addRequest(infoRequest).start();
+	}
 
 	render() {
 		return(
